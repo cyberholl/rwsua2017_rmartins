24c558dfd131ed576fd24e43adc1adcf125f6499
cyberholl <rgm@ua.pt>
2017-03-10 11:34:58 +0000

    update

--- player_rmartins/src/player_rmartins_node.cpp
+++ player_rmartins/src/player_rmartins_node.cpp
@@ -1,60 +1,11 @@
 #include <iostream>
 #include <vector>
-
+#include <rwsua2017_libs/player.h>
+#include <rwsua2017_msgs/MakeAPlay.h>
 using namespace std;
 
 namespace rwsua2017
 {
-class Player
-{
-    public:
-
-    Player(std::string argin_name, std::string argin_team_name="red")
-    {
-       
-	std::cout << "player name " << argin_name << std::endl;    
-        this->name = argin_name;
-	//this->team_name = argin_team_name;
-	set_team_name(argin_team_name);
-    }
-
-
-    std::string name;
-
-
-	// accessor (SET)
-  
-void set_team_name(string argin_team_name)
-{
-	if (argin_team_name =="red" || argin_team_name =="green" || argin_team_name =="blue" )
-{
-	this->team_name=argin_team_name;
-}
-	else
-{
-	cout<<"Error incorrect team name" << endl;
-}
-}
-
-
-void set_team_name(void)
-{
-set_team_name("blue");
-}
-	// accessor (GET)
-	string get_team_name(void)
-	{
-	return this->team_name;
-	};
-
-
-
-    	private:
-  	string team_name;
-
-};
-
-
 class MyPlayer: public Player
 {
 public:
@@ -65,7 +16,6 @@ cout<<"Initialized MyPlayer" << endl;
 };
 
 vector<string> teammates;
-
 
 };
 
