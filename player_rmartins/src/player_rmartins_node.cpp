512a2d59fd92f2f2a0418b7a7dd8c2e472b6334b
cyberholl <rgm@ua.pt>
2017-03-10 12:18:19 +0000

    Close #1. Create msg package.

--- player_rmartins/src/player_rmartins_node.cpp
+++ player_rmartins/src/player_rmartins_node.cpp
@@ -1,7 +1,14 @@
+//STD Inludes
 #include <iostream>
 #include <vector>
+
+//ROS includes
+#include <ros/ros.h>
+
+//
 #include <rwsua2017_libs/player.h>
 #include <rwsua2017_msgs/MakeAPlay.h>
+
 using namespace std;
 
 namespace rwsua2017
@@ -9,11 +16,24 @@ namespace rwsua2017
 class MyPlayer: public Player
 {
 public:
+// Variaveis têm que ser declaradas antes das funcões
+  ros::NodeHandle n;
+  ros::Subscriber sub;
+
 MyPlayer(string argin_name, string argin_team_name): Player(argin_name, argin_team_name)
 
 {  
+sub = n.subscribe("/make_a_play", 1000, &MyPlayer::makeAPlayCallback, this);
+
 cout<<"Initialized MyPlayer" << endl;
 };
+
+
+void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg)
+{
+cout << "received a make a play msg with max displacemente = " <<msg->max_displacement <<endl;   
+
+}
 
 vector<string> teammates;
 
@@ -25,8 +45,11 @@ vector<string> teammates;
 
 
 
-int main()
+int main(int argc, char **argv)
 {
+ ros::init(argc, argv, "player_rmartins");
+
+
     cout << "Hello world" << endl;   
    
     //rwsua2017::Player player("rmartins","blue"); 
@@ -44,9 +67,8 @@ cout << "Team"<<endl;
 for(size_t i=0; i<myplayer.teammates.size(); ++i)
 {
 cout << myplayer.teammates[i]<<endl;
-
-
 }
+ros::spin();// fica em constante funcionamento
 
     //cout << "Team Name =  " << player.team_name << endl;
     return 1;                                                      
