606c8701a072b45c83ecca643dcd9ddc475d61cf
cyberholl <rgm@ua.pt>
2017-03-17 14:51:14 +0000

    add

--- player_rmartins/src/player_rmartins_node.cpp
+++ player_rmartins/src/player_rmartins_node.cpp
@@ -197,7 +197,7 @@ public:
         marker.color.b = 0.3;
         marker.frame_locked = 1;
         marker.lifetime = ros::Duration(1);
-        marker.text = "baby don't hurt me";
+        marker.text = "baby dont hurt me";
         vis_pub.publish( marker ); //publicar o marcador
 
 
